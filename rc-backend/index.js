570db5f722e37a449e37469714c6439ff58ec551
Arshad khan <arshadkh507@gmail.com>
2023-06-18 08:48:52 +0500

    setup enviroment variable in vercel

--- rc-backend/index.js
+++ rc-backend/index.js
@@ -1,3 +1,32 @@
+const express = require("express");
+const connectDB = require("./db/db");
+const userRoutes = require("./routes/user");
+const cors = require("cors");
+
+const app = express();
+
+// Connect to MongoDB
+connectDB();
+
+app.use(express.json());
+
+// Enable CORS for all routes
+// const corsOptions = {
+//   origin: "https://testingroadmapcodecamp.netlify.app/",
+// };
+// app.use(cors(corsOptions));
+
+app.use(cors());
+
+// Use user routes
+app.use("/user", userRoutes);
+
+// Start server
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
+
 /* const serverless = require("serverless-http");
 const express = require("express");
 const connectDB = require("./db/db");
@@ -21,29 +50,3 @@ app.use("/user", userRoutes);
 
 // Export the Express app as a serverless function
 module.exports.handler = serverless(app); */
-
-const express = require("express");
-const connectDB = require("./db/db");
-const userRoutes = require("./routes/user");
-const cors = require("cors");
-
-const app = express();
-
-// Connect to MongoDB
-connectDB();
-
-app.use(express.json());
-// Enable CORS for all routes
-const corsOptions = {
-  origin: "https://testingroadmapcodecamp.netlify.app/",
-};
-app.use(cors(corsOptions));
-// Middleware
-// Use user routes
-app.use("/user", userRoutes);
-
-// Start server
-const PORT = process.env.PORT || 8000;
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
