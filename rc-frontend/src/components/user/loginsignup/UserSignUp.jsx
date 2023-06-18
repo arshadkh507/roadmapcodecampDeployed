e92d9933b3afb0e9c5735d330734f0ad3b9178a3
Arshad khan <arshadkh507@gmail.com>
2023-06-18 13:43:22 +0500

    usersignup fetch post mode changesd

--- rc-frontend/src/components/user/loginsignup/UserSignUp.jsx
+++ rc-frontend/src/components/user/loginsignup/UserSignUp.jsx
@@ -227,8 +227,9 @@ const UserSignUp = () => {
         console.log(url);
         const { name, email, password } = formData;
         const body = { name, email, password };
-        const response = await fetch(url, {
+        const response = await fetch(`${url}`, {
           method: 'POST',
+          mode: 'cors',
           headers: {
             'Content-Type': 'application/json',
           },
